1abcc87b52c9d5de3abcc3d74332d330ab34df9c
1+1=100 <95321232+Rubyian@users.noreply.github.com>
2021-11-30 20:53:26 -0300

    main.py "pode trocar o nome contanto que tenha .py no fim"
    Start to coding discord bots in python!

---
+++ main.py
@@ -0,0 +1,18 @@
+import discord  #importação da biblioteca do discord, se necessario a instale da seguinte maneira "pip install discord"
+from discord.ext import commands #importtação dos comandos. 
+
+client = commands.Bot(command_prefix = "{seu prefix}",case_insensitive = True) #defina seu prefix, e o "case_insensitive = True" define que seu comando sera lido mesmo em letra maiuscula ou minuscula.
+
+@client.event
+async def on_ready():  #Define os status do bot como onlie, o bot não funciona sem este evento.
+   print('Estou online') "print personalizavel.
+   
+@client.command() #define comandos.
+async def oi(ctx):
+   await ctx.send(f"Olá {ctx.author.mention}")
+   
+   
+client.run("{seu token}") #insira o token do seu bot.
+
+
+#Mais updates em breve!
