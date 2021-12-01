0dc5495136c622c72f2d2629534776fa2d4de47a
1+1=100 <95321232+Rubyian@users.noreply.github.com>
2021-11-30 21:11:44 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import discord  #importação da biblioteca do discord, se necessario a instale da seguinte maneira "pip install discord"
+import discord  #importação da biblioteca do discord, se necessario a instale da seguinte maneira "pip install discord" no seu terminal.
 from discord.ext import commands #importtação dos comandos. 
 
 client = commands.Bot(command_prefix = "{seu prefix}",case_insensitive = True) #defina seu prefix, e o "case_insensitive = True" define que seu comando sera lido mesmo em letra maiuscula ou minuscula.
